44667efa8e12db1d284f19ada1d425cffb2b2ce9
Ib Kunz Mathiesen <kunzmathiesen@hotmail.com>
2023-09-22 11:06:45 +0200

    Add loop exercises

---
+++ LoopExercises/LoopExercisesTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace LoopExercises;
+
+public class LoopExercisesTests
+{
+    private Loop_Exercises exercise;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        exercise = new Loop_Exercises();
+    }
+    
+    [Test]
+    public void TestSumOfNNumbers()
+    {
+        Assert.AreEqual(15, exercise.SumOfNNumbers(5));
+    }
+    
+    [Test]
+    public void TestFactorial()
+    {
+        Assert.AreEqual(120, exercise.Factorial(5));
+    }
+    
+    [Test]
+    public void TestFibonacciSeries()
+    {
+        CollectionAssert.AreEqual(new List<int> { 0, 1, 1, 2, 3 }, exercise.FibonacciSeries(5));
+    }
+    
+    [Test]
+    public void TestCountDigits()
+    {
+        Assert.AreEqual(5, exercise.CountDigits(12345));
+    }
+    
+    [Test]
+    public void TestReverseNumber()
+    {
+        Assert.AreEqual(54321, exercise.ReverseNumber(12345));
+    }
+    
+    [Test]
+    public void TestCalculatePower()
+    {
+        Assert.AreEqual(8, exercise.CalculatePower(2, 3));
+    }
+    
+    [Test]
+    public void TestMultiplicationTable()
+    {
+        CollectionAssert.AreEqual(new List<string> { "5x1=5", "5x2=10", "5x3=15" }, 
+            exercise.MultiplicationTable(5, 3));
+    }
+    
+    [Test]
+    public void TestSumOfDigits()
+    {
+        Assert.AreEqual(6, exercise.SumOfDigits(123));
+    }
+    
+    [Test]
+    public void TestIsArmstrong()
+    {
+        Assert.IsTrue(exercise.IsArmstrong(153));
+        Assert.IsFalse(exercise.IsArmstrong(123));
+    }
+
+    [Test]
+    public void TestPrimeNumbersUpToN()
+    {
+        CollectionAssert.AreEqual(new List<int> { 2, 3, 5, 7 }, 
+            exercise.PrimeNumbersUpToN(10));
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ LoopExercises/Loop_Exercises.cs
@@ -0,0 +1,115 @@
+﻿namespace LoopExercises;
+
+public class Loop_Exercises
+{
+    // Description: Write a method that calculates the sum of the first N numbers.
+    // Example Input: 5
+    // Example Output: 15 (1+2+3+4+5)
+    public int SumOfNNumbers(int n)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+    
+    // Description: Write a method that calculates the factorial of a number using loops.
+    // A factorial of a non-negative integer n is the product of all positive integers
+    // less than or equal to n.
+    // Example Input: 5
+    // Example Output: 120
+    public int Factorial(int num)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+    
+    // Description: Write a method that prints the first N numbers in the Fibonacci series.
+    // A Fibonacci number is a number in the sequence of numbers called the Fibonacci sequence,
+    // where each number after the first two is the sum of the two preceding ones.
+    // The sequence starts with 0 and 1, and the subsequent numbers in the sequence
+    // are formed by adding the previous two numbers.
+    // Fibonacci Sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+    // Example Input: 5
+    // Example Output: 0, 1, 1, 2, 3
+    public List<int> FibonacciSeries(int n)
+    {
+        // TODO: Implement this method
+        return new List<int>();
+    }
+    
+    // Description: Write a method that counts the number of digits in a number.
+    // Example Input: 12345
+    // Example Output: 5
+    public int CountDigits(int num)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    // Description: Write a method that reverses a number.
+    // Example Input: 12345
+    // Example Output: 54321
+    public int ReverseNumber(int num)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+    
+    // Description: Write a method that calculates the power of a number using loops.
+    // Example Input: 2, 3
+    // Example Output: 8
+    public int CalculatePower(int baseNum, int exponent)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+    
+    // Description: Write a method that prints the multiplication table for a number up to N.
+    // Example Input: 5, 3
+    // Example Output: 5x1=5, 5x2=10, 5x3=15
+    public List<string> MultiplicationTable(int num, int n)
+    {
+        // TODO: Implement this method
+        return new List<string>();
+    }
+    
+    // Description: Write a method that calculates the sum of digits of a number.
+    // Example Input: 123
+    // Example Output: 6
+    public int SumOfDigits(int num)
+    {
+        // TODO: Implement this method
+        return 0;
+    }
+    
+    // Description: Write a method that checks if a number is an Armstrong number.
+    // An Armstrong number (also known as a narcissistic number, pluperfect digit,
+    // or pluperfect number) is a number that is equal to the sum of its own digits
+    // raised to the power of the number of digits. In other words,
+    // an n-digit number is an Armstrong number if the sum of its digits,
+    // each raised to the nth power, is equal to the number itself.
+    // Example Input: 153
+    // Example Output: true (because 1^3 + 5^3 + 3^3 = 153)
+    public bool IsArmstrong(int num)
+    {
+        // TODO: Implement this method
+        return false;
+    }
+    
+    // Description: Write a method that finds all prime numbers up to N.
+    // Example Input: 10
+    // Example Output: 2, 3, 5, 7
+    public List<int> PrimeNumbersUpToN(int n)
+    {
+        // TODO: Implement this method
+        return new List<int>();
+    }
+
+
+
+
+
+
+
+
+
+}
